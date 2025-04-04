8c7bce539504f7e485063670b379332d5e6790b6
gonn <lgy0109@naver.com>
2025-04-04 15:20:41 +0900

    Initial commit

---
+++ ollama.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+import random
+
+st.write('Hi!')
+st.title("Make Your Own Chat Bot")
+
+st.header("This is a Header")
+
+st.subheader("subheader")
+
+st.text("text")
+
+st.write("default font")
+
+st.markdown("This is markdown")
+st.markdown("# This is markdown heading")
+st.markdown("## This is markdown subheading")
+
+name = st.text_area("Enter your name")
+
+button = st.button("Click Me")
+if button:
+    st.write("Clicked Btn")
+    
+json_data = {
+    "name" : "Test",
+    "age" : 30
+}
+
+df = pd.read_csv("data/example")
+
